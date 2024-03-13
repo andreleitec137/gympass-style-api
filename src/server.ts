d300b164a2c88f048e6c75fd71cda27c54179f60
André Leite <andreleite002@gmail.com>
2024-03-13 19:41:58 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP Server Running 🚀')
+  })
